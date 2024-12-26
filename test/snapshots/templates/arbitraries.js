6fab1ca019e5f6685ae5ac01d634995de909fe14
Tomer Aberbach <tomer@stainlessapi.com>
2024-12-25 19:58:33 -0500

    feat: basic templated type support (#76)

---
+++ test/snapshots/templates/arbitraries.js
@@ -0,0 +1,28 @@
+import * as fc from "fast-check";
+
+export const TemplateUnion = (a, b) =>
+  fc.oneof(
+    a,
+    b,
+  );
+
+export const $Model = fc.record({
+  instantiatedUnion: TemplateUnion(fc.integer(), fc.string()),
+});
+
+export const TemplateModel1 = (a, b) =>
+  fc.record({
+    a: a,
+    b: b,
+  });
+
+export const TemplateModel2 = (a, b) =>
+  fc.record({
+    property1: TemplateModel1(a, b),
+    property2: fc.oneof(
+      a,
+      b,
+    ),
+  });
+
+export const InstantiatedModel1 = TemplateModel1(fc.string(), fc.string());
