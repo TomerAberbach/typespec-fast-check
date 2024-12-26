a535d2a057f806e4f01b6c4b53639a6fc195d391
Tomer Aberbach <tomer@stainlessapi.com>
2024-12-26 01:24:07 -0500

    feat: openai example (#80)

--- test/snapshots/array/arbitraries.js
+++ test/snapshots/array/arbitraries.js
@@ -1,3 +1,5 @@
+// Generated from TypeSpec using `typespec-fast-check`
+
 import * as fc from "fast-check";
 
 export const $Array = fc.array(fc.string());
--- test/snapshots/float32/arbitraries.js
+++ test/snapshots/float32/arbitraries.js
@@ -1,3 +1,5 @@
+// Generated from TypeSpec using `typespec-fast-check`
+
 import * as fc from "fast-check";
 
 export const Float32 = fc.float();
